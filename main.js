6820938c07758e10cff5092c8d3cb0e37b08f27a
MEGAN ARNELL <arnell.megan@yahoo.com>
2017-02-07 14:10:37 -0700

    articles showing

---
+++ main.js
@@ -0,0 +1,39 @@
+$(document).ready(function() {
+  
+  var beginDate = "19920903";
+  var endDate = "19920904";
+
+  var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+  var articles = [];
+  
+  url += '?' + $.param({
+    'api-key': "36cb9403d4a8431bac2ea5a69539ff53",
+    'begin_date': beginDate,
+    'end_date': endDate
+  });
+  $.ajax({
+    url: url,
+    method: 'GET',
+  }).done(function(result) {
+    articles = result['response']['docs'];
+    var articleContent = "";
+    for(var i=0; i<articles.length; i++){
+      var article = articles[i];
+      var headline = article['headline']['main'];
+      var snippet = article['snippet'];
+      var pictureSrc = "";
+      if(article['multimedia'].length == 0){
+        pictureSrc = "nyt-logo.png";
+      }
+      else {
+        pictureSrc = article['multimedia'][0];
+      }
+      articleContent += "<div class='media'><div class='media-left'><a href='#'><img class='media-object' src='" + pictureSrc + "' height='50px'></a></div><div class='media-body'><h4 class='media-heading'>" + headline + "</h4>" + snippet + " <a href=''>View More</a></div></div>";
+      
+    }
+    $('#articles').html(articleContent);
+    console.log(result);
+  }).fail(function(err) {
+    throw err;
+  });
+});
